9220b5a9e2620d8d3f7e00f54113e3fd65011879
vkay616 <vinaykhanduri6@gmail.com>
2023-05-19 18:33:38 +0530

    Implemented deal function and learnt about returning multiple values

--- cards/main.go
+++ cards/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	// var card string = "Ace of Spades"
 	// card := newCard()
@@ -8,7 +10,16 @@ func main() {
 	// cards = append(cards, "Five of Club")
 	cards := newDeck()
 
+	hand, remainingCards := deal(cards, 10)
+
+	fmt.Println("All Cards:")
 	cards.print()
+
+	fmt.Println("Cards in Hand after dealing:")
+	hand.print()
+
+	fmt.Println("Remaining cards in pile:")
+	remainingCards.print()
 
 	// for i, card := range cards {
 	// 	fmt.Println(i, card)
