89be7236c442b9636633bf17a7c8d9f759f6dbf4
Vinay Khanduri <vinaykhanduri2001@gmail.com>
2023-05-19 15:46:50 +0530

    Learnt about Slices and For Loops in Go

--- cards/main.go
+++ cards/main.go
@@ -4,8 +4,16 @@ import "fmt"
 
 func main() {
 	// var card string = "Ace of Spades"
-	card := newCard()
-	fmt.Println(card)
+	// card := newCard()
+	// fmt.Println(card)
+	cards := []string{newCard(), "Ace of Diamonds", newCard()}
+	cards = append(cards, "Five of Club")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
+	// fmt.Println(cards)
 }
 
 func newCard() string {
