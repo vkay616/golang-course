3e49164171510cbe548a1d03ecc7135bc725065f
Vinay Khanduri <vinaykhanduri2001@gmail.com>
2023-05-19 14:57:14 +0530

    Learnt about functions in Go

--- cards/main.go
+++ cards/main.go
@@ -4,6 +4,10 @@ import "fmt"
 
 func main() {
 	// var card string = "Ace of Spades"
-	card := "Ace of Spades"
+	card := newCard()
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Ace of Hearts"
 }
