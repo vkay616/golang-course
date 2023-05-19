45f4b2c8bcfa3edd6a3f424535840a1f0302fc1d
vkay616 <vinaykhanduri6@gmail.com>
2023-05-19 19:43:03 +0530

    Implemented saveToFile and loadDeckFromFile functions

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
 // custom type deck
 type deck []string
@@ -28,4 +33,23 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func loadDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		os.Exit(1)
+	}
+
+	return deck(strings.Split(string(bs), ","))
 }
--- cards/main.go
+++ cards/main.go
@@ -1,25 +1,31 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Ace of Spades"
 	// card := newCard()
 	// fmt.Println(card)
 	// cards := deck{newCard(), "Ace of Diamonds", newCard()}
 	// cards = append(cards, "Five of Club")
-	cards := newDeck()
+	// cards := newDeck()
 
-	hand, remainingCards := deal(cards, 10)
+	// fmt.Println(cards.toString())
 
-	fmt.Println("All Cards:")
+	// cards.saveToFile("saved_cards")
+
+	cards := loadDeckFromFile("saved_cards")
+
 	cards.print()
 
-	fmt.Println("Cards in Hand after dealing:")
-	hand.print()
+	// hand, remainingCards := deal(cards, 10)
 
-	fmt.Println("Remaining cards in pile:")
-	remainingCards.print()
+	// fmt.Println("All Cards:")
+	// cards.print()
+
+	// fmt.Println("Cards in Hand after dealing:")
+	// hand.print()
+
+	// fmt.Println("Remaining cards in pile:")
+	// remainingCards.print()
 
 	// for i, card := range cards {
 	// 	fmt.Println(i, card)
