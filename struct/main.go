173d584e0f22656cd8cdd6f5394c7c5e3809d5df
vkay616 <vinaykhanduri6@gmail.com>
2023-05-21 01:42:55 +0530

    Learnt about Pointers in Go and implemented a struct update function using pointers

--- struct/main.go
+++ struct/main.go
@@ -10,7 +10,7 @@ type contactInfo struct {
 type person struct {
 	firstName string
 	lastName  string
-	contact   contactInfo
+	contactInfo
 }
 
 func main() {
@@ -27,11 +27,33 @@ func main() {
 	john := person{
 		firstName: "John",
 		lastName:  "Wick",
-		contact: contactInfo{
+		contactInfo: contactInfo{
 			email:   "johnwick@gmail.com",
 			zipCode: 293525,
 		},
 	}
 
-	fmt.Printf("%+v", john)
+	john.updatePersonInfo()
+
+	john.print()
+}
+
+func (pointerToPerson *person) updatePersonInfo() {
+
+	var fn, ln, e string
+	var z int
+
+	fmt.Printf("Write the updated information in the following order:\nFirst_Name Last_Name Email Zip_Code\n")
+	fmt.Scanf("%v %v %v %v", &fn, &ln, &e, &z)
+
+	(*pointerToPerson).firstName = fn
+	(*pointerToPerson).lastName = ln
+	(*pointerToPerson).contactInfo.email = e
+	(*pointerToPerson).contactInfo.zipCode = z
+
+}
+
+func (p person) print() {
+	fmt.Printf("First Name: %v\nLast Name: %v\nEmail: %v\nZip Code: %v", p.firstName,
+		p.lastName, p.contactInfo.email, p.contactInfo.zipCode)
 }
