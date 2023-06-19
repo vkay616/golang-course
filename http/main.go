06bd1735e68618441fd34ef95c89bf804da45e67
vkay616 <vinaykhanduri6@gmail.com>
2023-06-19 19:00:26 +0530

    learnt about Reader and Writer interface

--- http/main.go
+++ http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("https://www.google.com")
@@ -13,5 +16,17 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("No. of bytes in byte slice:", len(bs))
+
+	return len(bs), nil
 }
